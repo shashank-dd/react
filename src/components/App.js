04fed5b7a2affe6606ec78383017bb6d114cb759
shashank-dd <shashankdd1805@gmail.com>
2022-12-10 18:26:44 +0530

    hhh

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+import Form from "./form";
+
+const App = () => {
+  return (
+
+    <div id="main">
+
+  <Form/>
+    
+    
+    </div>
+  )
+}
+
+
+export default App;
